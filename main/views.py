f0d05a475170a0b7c67ee22d954e5e9dd1c79270
reyhanardian16 <reyhanardian16@gmail.com>
2024-09-11 11:35:45 +0700

    update html

--- main/views.py
+++ main/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
+        'appname' : 'naiki-apparel',
         'npm' : '2306217185',
         'name': 'Muhammad Reyhan Ardian',
         'class': 'PBP D'
