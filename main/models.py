8583bf4741f513215b09cad022d9096ad4aaf0c8
reyhanardian16 <reyhanardian16@gmail.com>
2024-09-11 10:58:03 +0700

    new model

--- main/models.py
+++ main/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 
-class MoodEntry(models.Model):
-    mood = models.CharField(max_length=255)
-    time = models.DateField(auto_now_add=True)
-    feelings = models.TextField()
-    mood_intensity = models.IntegerField()
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+    size = models.CharField(max_length=255)
+    description = models.TextField()
 
-    @property
-    def is_mood_strong(self):
-        return self.mood_intensity > 5
+
+    
--- main/views.py
+++ main/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
-        'npm' : '2306123456',
-        'name': 'Pak Bepe',
-        'class': 'PBP E'
+        'npm' : '2306217185',
+        'name': 'Muhammad Reyhan Ardian',
+        'class': 'PBP D'
     }
 
     return render(request, "main.html", context)
