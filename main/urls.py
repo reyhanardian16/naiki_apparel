83f70543aae1b8a13011777595f168234f0fb01b
reyhanardian16 <reyhanardian16@gmail.com>
2024-09-18 11:57:28 +0700

    add forms

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
 from main.views import show_main, create_product_entry, show_xml, show_json, show_xml_by_id, show_json_by_id
 
-
-
-
 app_name = 'main'
 
 urlpatterns = [
